7555463d5781c7e36160ae7edf37e55fed970235
Charlie Daly <the_charlie_daly@hotmail.co.uk>
2017-10-12 00:47:37 +0100

    Added env module

---
+++ test/Core.Test/EnvTests.fs
@@ -0,0 +1,50 @@
+module EnvTests
+
+open Env
+open Parser
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let ``Get nonexistent ref returns none`` () = 
+    defaultEnvironment "na" |> should equal None
+
+[<Fact>]
+let ``Get existent ref returns value`` () = 
+    let env = Env.add "ref" (Atom(Int 0)) defaultEnvironment
+    env "ref" |> should equal (Some(Atom(Int 0)))
+
+[<Fact>]
+let ``Get returns most recent value`` () = 
+    let env = defaultEnvironment |> Env.add "ref" (Atom(Int 0)) |> Env.add "ref" (Atom(Int 1))
+    env "ref" |> should equal (Some(Atom(Int 1)))
+    
+[<Fact>]
+let ``Fold over empty arg list returns empty env`` () =
+    let env = Env.foldArgs (Atom Nil) (Atom Nil) defaultEnvironment
+    env "na" |> should equal None
+
+[<Fact>]
+let ``Fold over single arg returns arg when called`` () = 
+    let labels = Tree(Atom(Ref "x"), Atom Nil)
+    let values = Tree(Atom(Int 0), Atom Nil)
+    let env = Env.foldArgs labels values defaultEnvironment
+    env "x" |> should equal (Some(Atom(Int 0)))
+
+[<Fact>]
+let ``Fold over multi args returns args when called`` () = 
+    let labels = 
+        Tree(
+            Atom(Ref "x"), 
+            Tree(
+                Atom(Ref "y"),
+                Atom(Nil)))
+    let values = 
+        Tree(
+            Atom(Int 0), 
+            Tree(
+                Atom(Int 1),
+                Atom(Nil)))
+    let env = Env.foldArgs labels values defaultEnvironment
+    env "x" |> should equal (Some(Atom(Int 0)))
+    env "y" |> should equal (Some(Atom(Int 1)))
--- test/Core.Test/EvalTests.fs
+++ test/Core.Test/EvalTests.fs
@@ -1,25 +1,43 @@
 module EvalTests
 
 open Eval
+open Env
 open Parser
 open Xunit
 open FsUnit.Xunit
 
 [<Fact>]
 let ``Eval atom returns atom`` () = 
-    let actual = Atom(Int 0) |> Eval.run
+    let actual = Atom(Int 0) |> Eval.run defaultEnvironment
     let expected = Atom(Int 0)
     actual |> should equal expected
 
 [<Fact>]
 let ``Eval tree of (Atom, Nil)`` () = 
-    let actual = Tree(Atom(Int 0), Atom(Nil)) |> Eval.run
+    let actual = Tree(Atom(Int 0), Atom(Nil)) |> Eval.run defaultEnvironment
     let expected = Atom(Int 0)
     actual |> should equal expected
 
 [<Fact>]
-let ``Eval tree of (Atom, Atom), no environment`` () =
-    let actual = Tree(Atom(Int 0), Atom(Int 1)) |> Eval.run
+let ``Eval tree of (tree, tree), no environment`` () =
+    let actual = Tree(Atom(Int 0), Atom(Int 1)) |> Eval.run defaultEnvironment
     let expected = Tree(Atom(Int 0), Atom(Int 1))
     actual |> should equal expected
-    
+
+[<Fact>]
+let ``Eval reference to value`` () = 
+    let expected = Atom(Int 0)
+    let env = Env.add "x" expected defaultEnvironment
+    let actual = (Atom (Ref "x")) |> Eval.run env
+    actual |> should equal expected
+
+// [<Fact>]
+let ``Eval identity function`` () = 
+    let actual = 
+        Tree(
+            Atom(Ref "id"), 
+            Tree(
+                Atom(Int 0), 
+                Atom Nil)) |> Eval.run defaultEnvironment
+    let expected = Atom (Int 0)
+    actual |> should equal expected
